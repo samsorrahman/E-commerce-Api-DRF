28cb7373c351de7bcfd223c63289f605bc089371
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-02 12:54:59 +0500

    creating model serializer

--- store/serializers.py
+++ store/serializers.py
@@ -4,35 +4,54 @@ from decimal import Decimal
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source='unit_price')
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(
+#         max_digits=6, decimal_places=2, source='unit_price')
+#     price_with_tax = serializers.SerializerMethodField(
+#         method_name='calculate_tax')
+
+#     # returning id of each colelction(primary key method)
+#     # collection = serializers.PrimaryKeyRelatedField(
+#     #     queryset=Collection.objects.all()
+#     # )
+
+#     # return each collection name(string method)
+
+#     # collection = serializers.StringRelatedField()
+#     # returngin nested(nested objects methods)
+#     # collection = CollectionSerializer()
+
+#     # rendering links to each collection(hyperlink method)
+#     collection = serializers.HyperlinkedRelatedField(
+#         queryset=Collection.objects.all(),
+#         view_name='collection-detail'
+#     )
+
+#     def calculate_tax(self, product: Product):
+#         return product.unit_price*Decimal(1.1)
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+# creating model serializers
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
-
-    # returning id of each colelction
-    # collection = serializers.PrimaryKeyRelatedField(
-    #     queryset=Collection.objects.all()
-    # )
-
-    # return each collection name
-
-    # collection = serializers.StringRelatedField()
-    # returngin nested
-    # collection = CollectionSerializer()
-
-    # rendering links to each collection
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail'
-    )
 
     def calculate_tax(self, product: Product):
         return product.unit_price*Decimal(1.1)
