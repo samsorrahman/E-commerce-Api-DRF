c4c57b0ff0c4e02327688547ba18870ddab7784e
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-02 11:59:55 +0500

    product and products revealing

--- store/views.py
+++ store/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .serializers import ProductSerializer
+from .models import Product
 # Create your views here.
 
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
