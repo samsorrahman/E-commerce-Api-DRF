2ca5a77b7bcc87b9c0e70e341464d40e4c70fcd4
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-04 15:20:34 +0500

    authentication section completed

--- store/models.py
+++ store/models.py
@@ -76,6 +76,11 @@ class Order(models.Model):
         (PAYMENT_STATUS_FAILED, 'Failed')
     ]
 
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
+
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
