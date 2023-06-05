1dedce9ed274c8816d908036187921df574f8d43
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-05 07:54:08 +0500

    getting current user using modeheader chrextension

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,12 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as UserBaseSerializer, UserCreateSerializer as BaseUserCreateSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
                   'email', 'first_name', 'last_name']
+
+
+class UserSerializer(UserBaseSerializer):
+    class Meta(UserBaseSerializer):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
--- storefront/settings.py
+++ storefront/settings.py
@@ -157,6 +157,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
 AUTH_USER_MODEL = 'core.User'
 
@@ -164,8 +165,4 @@ DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer'
     }
-}
-
-SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
